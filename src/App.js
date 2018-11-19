9f8c0f75412f566957774585667f6c2063965e01
atsushi0623itoh <atsushi0623itoh@gmail.com>
2018-11-19 19:16:09 +0900

    create Img component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Balloon from './components/atoms/Balloon';
+import Img from './components/atoms/Img';
 
 // class App extends Component {
 //   render() {
@@ -9,6 +10,13 @@ import Balloon from './components/atoms/Balloon';
 //   }
 // }
 
-const App = () => <Balloon>test</Balloon>
+const App = () => {
+  return (
+    <div>
+      <Balloon>test</Balloon>
+      <Img src='https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-1/c0.0.320.320/p320x320/13631654_879887108807235_2829615063363236677_n.jpg?_nc_cat=101&_nc_ht=scontent-nrt1-1.xx&oh=28bc5da8151830b3e881a150a7fdf4ac&oe=5C6C3AED' width='128' height='72' />
+    </div>
+  )
+}
 
 export default App;
