e1f33c0536225f39aef7d02c8f6c50ab78fede8b
atsushi0623itoh <atsushi0623itoh@gmail.com>
2018-11-19 12:00:24 +0900

    test CSSModule

---
+++ src/components/atoms/Balloon/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './styles.module.sass';
+
+const Balloon = () => <span className={ styles.balloon }>削除する</span>;
+// const Balloon = () => <span>削除する</span>;
+
+export default Balloon;
