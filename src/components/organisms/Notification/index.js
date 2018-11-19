66fef3fd9ee3dc06c1bd242f18b319e4f6c7e052
atsushi0623itoh <atsushi0623itoh@gmail.com>
2018-11-20 07:09:14 +0900

    split Notification component to presenter and container

--- src/components/organisms/Notification/index.js
+++ src/components/organisms/Notification/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './styles.module.sass';
 import Img from '../../atoms/Img';
 import Heading from '../../atoms/Heading';
 import { InfoTxt } from '../../atoms/Text';
 import DeleteButton from '../../molecules/DeleteButton';
 
-const Notification = ({
+export const NotificationPresenter = ({
   program,
   className,
   onClickDelete,
@@ -21,6 +21,32 @@ const Notification = ({
       <DeleteButton onClick={ onClickDelete } className={ styles.del } />
     </div>
   </section>
+);
+
+export class NotificationContainer extends Component {
+  constructor() {
+    super();
+    this.onClickDelete = this.onClickDelete.bind(this);
+  }
+
+  render() {
+    const { presenter, onClickDelete: propsOnClickDelete, ...props } = this.props;
+    const onClickDelete = propsOnClickDelete ? this.onClickDelete : null;
+    const presenterProps = { onClickDelete, ...props };
+    return presenter(presenterProps);
+  }
+
+  onClickDelete(...args) {
+    const { onClickDelete, program } = this.props;
+    onClickDelete(...args, program);
+  }
+}
+
+const Notification = props => (
+  <NotificationContainer
+    presenter={ presenterProps => <NotificationPresenter { ...presenterProps } /> }
+    { ...props }
+  />
 );
 
 export default Notification
