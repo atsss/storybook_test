e0782a91b47f0ab74d27ac466cc2741408c9f613
atsushi0623itoh <atsushi0623itoh@gmail.com>
2018-11-19 19:40:35 +0900

    refactor Heading component to reduce unneccesary codes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Balloon from './components/atoms/Balloon';
 import Img from './components/atoms/Img';
-import { Heading1 } from './components/atoms/Heading';
+import Heading from './components/atoms/Heading';
 
 // class App extends Component {
 //   render() {
@@ -16,7 +16,7 @@ const App = () => {
     <div>
       <Balloon>test</Balloon>
       <Img src='https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-1/c0.0.320.320/p320x320/13631654_879887108807235_2829615063363236677_n.jpg?_nc_cat=101&_nc_ht=scontent-nrt1-1.xx&oh=28bc5da8151830b3e881a150a7fdf4ac&oe=5C6C3AED' width='128' height='72' />
-      <Heading1>aaaa</Heading1>
+      <Heading>aaaa</Heading>
     </div>
   )
 }
--- src/stories/index.js
+++ src/stories/index.js
@@ -5,6 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import Balloon from '../components/atoms/Balloon'
+import Heading from '../components/atoms/Heading'
 import Img from '../components/atoms/Img'
 
 storiesOf('Balloon', module)
@@ -14,3 +15,8 @@ storiesOf('Balloon', module)
 
 storiesOf('Img', module)
   .add('デフォルト', () => <Img src='https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-1/c0.0.320.320/p320x320/13631654_879887108807235_2829615063363236677_n.jpg?_nc_cat=101&_nc_ht=scontent-nrt1-1.xx&oh=28bc5da8151830b3e881a150a7fdf4ac&oe=5C6C3AED' width='128' height='72' />);
+
+storiesOf('Heading', module)
+  .add('デフォルト', () => <Heading>見出し</Heading>)
+  .add('レベル1', () => <Heading level={ 1 }>見出しレベル1</Heading>)
+  .add('レベル1, 見た目2', () => <Heading level={ 1 } visualLevel={ 2 }>見出しレベル1, 見た目2</Heading>);
